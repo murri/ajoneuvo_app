cbfd538f7dcb6dad89692cfa61c2637506c0db60
Tommi <tommi.muranen@agilopartners.fi>
2011-01-25 18:47:20 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjoneuvoApp::Application.config.secret_token = '8561f94d9f678eeef789c05843668829b45ddcf1640a1a960b0962c416be260c66bb72104162a0f1e684349207a1a8adb7b7adc034b364bb79989107e9369031'
